88eda3523856fb9789b41289d805253bba3b17fc
Steve <anil.stephen66@gmail.com>
2026-01-07 11:15:07 +0530

    Reset the position of copy-prompt button

--- widgets/so-ai-prompt/templates/template.php
+++ widgets/so-ai-prompt/templates/template.php
@@ -3,7 +3,11 @@
   $markdown_prompt = $instance['markdown_prompt'] ? $instance['markdown_prompt'] : "";
 ?>
 <div class="wtp-ai-prompt prompt-code" data-behaviour="wtp-ai-prompt">
-  <div class="wtp-html-prompt"><?php echo $html_prompt; ?></div>
+  <div class="wtp-html-prompt">
+    <p class="copy-wtp-markdown-prompt-wrapper text-right">
+      <button type="button" class="copy-wtp-markdown-prompt">Copy Prompt</button>
+    </p>
+    <?php echo $html_prompt; ?>
+  </div>
   <div class="wtp-markdown-prompt" style="display:none;"><?php echo $markdown_prompt; ?></div>
-  <button type="button" class="copy-wtp-markdown-prompt">Copy Prompt</button>
 </div>
